a2ca9133aa6bcb52f8166f7d22bc68bb958075fc
ABHISHEK Dhakad <85424422+CodeWithAbhishekDhakad@users.noreply.github.com>
2026-07-07 23:15:26 +0530

    Add files via upload

--- AssetsData/assets/js/config.js
+++ AssetsData/assets/js/config.js
@@ -7,6 +7,9 @@ const APP_CONFIG = {
   DEMO_PASSWORD: "password",
   SUPABASE_URL: "https://nffhnpmalprochrbnonn.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5mZmhucG1hbHByb2NocmJub25uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMxNTMxMjUsImV4cCI6MjA5ODcyOTEyNX0.6HuqRBC1MK8TZRtJL-FDFEWaXzZkB_jxIGMfPlVMSkU",
+  EMAIL_FUNCTION: "send-pm-email",
+  EMAIL_FROM: "IT Asset PM <onboarding@resend.dev>",
+  NOTIFY_EMAIL: "adhakad484@gmail.com",
   COMPANY_NAME: "CodeWithAbhishekDhakad",
   ASSET_TYPES: ["Laptop", "Desktop", "Printer", "Switch", "Access Point", "CCTV", "Server", "Telephone", "UPS", "Firewall", "Other"]
 };
